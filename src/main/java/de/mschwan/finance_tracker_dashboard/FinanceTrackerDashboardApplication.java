1f770bba40768e7d77ffd6ac9a49c6c5bc85f66e
mschwan <mschwan@stud.hs-heilbronn.de>
2026-04-13 17:58:19 +0200

    Initial commit: Spring Boot project structure

---
+++ src/main/java/de/mschwan/finance_tracker_dashboard/FinanceTrackerDashboardApplication.java
@@ -0,0 +1,13 @@
+package de.mschwan.finance_tracker_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mschwan/finance_tracker_dashboard/FinanceTrackerDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mschwan.finance_tracker_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
