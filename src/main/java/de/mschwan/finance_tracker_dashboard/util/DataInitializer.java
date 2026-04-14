8170949f927c48eaa1132a6b42019f63348f8897
mschwan <mschwan@stud.hs-heilbronn.de>
2026-04-14 16:13:55 +0200

    feat: update default categories and add http test requests

--- src/main/java/de/mschwan/finance_tracker_dashboard/util/DataInitializer.java
+++ src/main/java/de/mschwan/finance_tracker_dashboard/util/DataInitializer.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataInitializer implements CommandLineRunner {
 
+  private static final String COLOR_INCOME = "#4CAF50";
+  private static final String COLOR_HOUSING = "#F44336";
+  private static final String COLOR_GROCERIES = "#FF9800";
+  private static final String COLOR_LEISURE = "#9C27B0";
+  private static final String COLOR_MOBILITY = "#2196F3";
+  private static final String COLOR_SHOPPING = "#E91E63";
+  private static final String COLOR_INVESTMENT = "#00BCD4";
+
   private final CategoryRepository categoryRepository;
 
   public DataInitializer(CategoryRepository categoryRepository) {
@@ -18,22 +26,26 @@ public class DataInitializer implements CommandLineRunner {
   @Override
   public void run(String... args) {
     if (categoryRepository.count() == 0) {
-      System.out.println("Erstelle Test-Kategorien...");
+      // INCOME
+      createCategory("SALARY", CategoryType.INCOME, COLOR_INCOME);
+      createCategory("GIFTS", CategoryType.INCOME, COLOR_INCOME);
+      createCategory("REFUNDS", CategoryType.INCOME, COLOR_INCOME);
 
-      Category gehalt = new Category();
-      gehalt.setName("Gehalt");
-      gehalt.setType(CategoryType.INCOME);
-      gehalt.setColorHex("#4CAF50");
-
-      Category miete = new Category();
-      miete.setName("Miete");
-      miete.setType(CategoryType.EXPENSE);
-      miete.setColorHex("#F44336");
-
-      categoryRepository.save(gehalt);
-      categoryRepository.save(miete);
-
-      System.out.println("Test-Kategorien erfolgreich gespeichert!");
+      // EXPENSE
+      createCategory("HOUSING", CategoryType.EXPENSE, COLOR_HOUSING);
+      createCategory("GROCERIES", CategoryType.EXPENSE, COLOR_GROCERIES);
+      createCategory("LEISURE", CategoryType.EXPENSE, COLOR_LEISURE);
+      createCategory("MOBILITY", CategoryType.EXPENSE, COLOR_MOBILITY);
+      createCategory("SHOPPING", CategoryType.EXPENSE, COLOR_SHOPPING);
+      createCategory("INVESTMENTS", CategoryType.EXPENSE, COLOR_INVESTMENT);
     }
+  }
+
+  private void createCategory(String name, CategoryType type, String color) {
+    Category category = new Category();
+    category.setName(name);
+    category.setType(type);
+    category.setColorHex(color);
+    categoryRepository.save(category);
   }
 }
